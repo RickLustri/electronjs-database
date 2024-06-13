2e142c28e3720cde9c12352282a9d3cb92460303
rkzin <rkzinnine@gmail.com>
2024-06-12 22:23:19 -0300

    elesctron e banco de dados

---
+++ src/script.js
@@ -0,0 +1,54 @@
+// Criando uma função para registrar um novo usuário
+function registrarUsuario(event) {
+
+  // Previne o comportamento padrão do formulário
+  event.preventDefault();
+
+  // Puxando os dados do formulário
+  var nome = document.getElementById('name').value;
+  var sobrenome = document.getElementById('sobrenome').value;
+  var cidade = document.getElementById('cidade').value;
+
+  // Mostrando os dados no console
+  console.log('Nome:', nome, '\nSobrenome:', sobrenome, '\nCidade:', cidade);
+
+  // Puxando a base de dados
+  var mysql = require('mysql2');
+
+  // Criando a conexão com o MySQL
+  var conexao = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'electron'
+  });
+
+  // Verificando se a conexão teve erros
+  conexao.connect(function (error) {
+    if (error) {
+      console.log(error.code);
+      console.log(error.fatal);
+    };
+  });
+
+  // Inserindo os dados do formulário no MySQL
+  var query = `INSERT INTO pessoa (nome, sobrenome, cidade) VALUES ("${nome}","${sobrenome}","${cidade}")`;
+  
+  // Executando a query no MySQL
+  conexao.query(query, function (error) {
+    
+    // Verificando se houve erros ao registrar o usuário
+    if (error) {
+      console.log("Erro ao registrar o usuário:", error);
+    }else {
+      console.log("Usuário registrado com sucesso!");
+    };
+
+  });
+
+}
+
+// Puxando o formulário e adicionando um evento de submit do botão
+var formulario = document.getElementById('formulario');
+formulario.addEventListener('submit', registrarUsuario);
+
